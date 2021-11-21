d0fca731e7973c6f6b122babfa7dd40978369a01
BaranovArtyom <87430808+BaranovArtyom@users.noreply.github.com>
2021-11-21 17:45:43 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -13,3 +13,14 @@ multi_obj = multi_yield()
 # print(next(multi_obj))
 for my_string in multi_obj:
     print(my_string)
+
+# последовательность из квадратов чисел от 1 до N (1 ** 2, 2 ** 2, 3 **2 и так далее).(Генератор)
+
+def multi_yield(limit):
+     for i in range(1, limit+1):
+         yield i ** 2
+
+     
+multi_obj = multi_yield(5)
+for my_string in multi_obj:
+    print(my_string)
